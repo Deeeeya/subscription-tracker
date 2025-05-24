7f2c53e964763fbd658cacce63b5895ff63a10cd
Deeeeya <tankonated@gmail.com>
2025-05-24 11:44:13 -0400

    feat(models): added subscription and user models

---
+++ models/user.model.js
@@ -0,0 +1,32 @@
+import mongoose from 'mongoose'
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "User Name is required"],
+        trim: true,
+        minlength: 2,
+        MaxLength: 50,
+    },
+    email: {
+        type: String,
+        required: [true, "Email is required"],
+        unique: true,
+        trim: true,
+        lowercase: true,
+        minlength: 5,
+        maxLength: 255,
+        match: [/\S+@\S+\.\S+/, 'Please fill a valid email address'],
+    },
+    password: {
+        type: String,
+        required: [true, "User Password is required"],
+        minLength: 6,
+    }
+}, { timestamps: true})
+
+const User = mongoose.model('User', userSchema)
+
+export default User;
+
+// { name: 'John Doe', email: 'johnney@email.com', password: 'password' }
